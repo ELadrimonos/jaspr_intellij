fdee2fe2c000a9aa408569665c9d5670151785e5
Adrián Primo <adrianprimob@gmail.com>
2026-02-25 19:41:45 +0100

    :pushpin: Pin max IDEA version (untilBuild), as Dart plugin is not compatible with 261, which is causing workflows to fail

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,6 +72,7 @@ intellijPlatform {
 
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
+            untilBuild = providers.gradleProperty("pluginUntilBuild")
         }
     }
 
